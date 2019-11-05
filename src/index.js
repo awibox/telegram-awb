d56fbb16078e174d0519525e2c8f0b5d67e09f4d
Andrei Arkhipov <andrei@awb.pw>
2019-11-06 00:54:04 +0300

    #15 TelegramApi and jquery was added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-document.write('Hello world');
+import {apiConfig} from 'config/api';
+require('telegram-api-js/dist/telegramApi');
+$( document ).ready(function() {
+  telegramApi.setConfig(apiConfig);
+  telegramApi.getUserInfo().then(function(user) {
+    if (user.id) {
+      // You have already signed in
+    } else {
+      // Log in
+    }
+  });
+  console.log('Worked')
+});
 
