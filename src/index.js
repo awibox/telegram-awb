7dc08ea06f2087e5b6e6aa1c351a8c34ab813d0b
Andrei Arkhipov <andrei@awb.pw>
2019-11-06 22:53:34 +0300

    #9 Router was created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import {apiConfig, TdClientOptions} from 'config/api';
 import TdClient from 'tdweb';
+import Router from 'router/router';
+import Route from 'router/route';
 
-const client = new TdClient(TdClientOptions);
-
-client.send({
-  '@type': 'setTdlibParameters',
-  parameters: apiConfig
-});
+(function () {
+  function init() {
+    const router = new Router([
+      new Route('login', 'login.html', true),
+      new Route('im', 'im.html')
+    ]);
+    console.log('router', router);
+    const client = new TdClient(TdClientOptions);
+    client.send({
+      '@type': 'setTdlibParameters',
+      parameters: apiConfig
+    });
+  }
+  init();
+}());
