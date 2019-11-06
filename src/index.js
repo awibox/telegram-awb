3785fd63ddb9d1c1132fa25143525cbc2005429d
Andrei Arkhipov <andrei@awb.pw>
2019-11-06 22:20:12 +0300

    #21 Utils was added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import {apiConfig} from 'config/api';
+import {apiConfig, TdClientOptions} from 'config/api';
 import TdClient from 'tdweb';
 
-const client = new TdClient(apiConfig);
+const client = new TdClient(TdClientOptions);
+
 client.send({
   '@type': 'setTdlibParameters',
-  api_id: apiConfig.app.id,
-  api_hash: apiConfig.app.hash,
-  system_language_code: navigator.language || 'en',
-  use_secret_chats: false,
-  use_message_database: true,
-  use_file_database: false,
-  database_directory: '/db',
-  files_directory: '/'
+  parameters: apiConfig
 });
