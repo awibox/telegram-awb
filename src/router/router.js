0e31eba8ca54da4871d3b4d06e126155786894e2
Andrei Arkhipov <andrei@awb.pw>
2019-11-11 20:28:34 +0300

    #59: Callback for router

--- src/router/router.js
+++ src/router/router.js
@@ -1,63 +1,72 @@
 'use strict';
 
 function Router(routes) {
-    try {
-        if (!routes) {
-            throw 'error: routes param is mandatory';
-        }
-        this.constructor(routes);
-        this.init();
-    } catch (e) {
-        console.error(e);   
+  try {
+    if (!routes) {
+      throw 'error: routes param is mandatory';
     }
+    this.constructor(routes);
+    this.init();
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 Router.prototype = {
-    routes: undefined,
-    rootElem: undefined,
-    constructor: function (routes) {
-        this.routes = routes;
-        this.rootElem = document.getElementById('app');
-    },
-    init: function () {
-        var r = this.routes;
-        (function(scope, r) { 
-            window.addEventListener('hashchange', function (e) {
-                scope.hasChanged(scope, r);
-            });
-        })(this, r);
-        this.hasChanged(this, r);
-    },
-    hasChanged: function(scope, r){
-        if (window.location.hash.length > 0) {
-            for (var i = 0, length = r.length; i < length; i++) {
-                var route = r[i];
-                if(route.isActiveRoute(window.location.hash.substr(1))) {
-                    scope.goToRoute(route.htmlName);
-                }
-            }
-        } else {
-            for (var i = 0, length = r.length; i < length; i++) {
-                var route = r[i];
-                if(route.default) {
-                    scope.goToRoute(route.htmlName);
-                }
-            }
+  routes: undefined,
+  rootElem: undefined,
+  constructor: function (routes) {
+    this.routes = routes;
+    this.rootElem = document.getElementById('app');
+  },
+  init: function () {
+    var r = this.routes;
+    (function (scope, r) {
+      window.addEventListener('hashchange', function (e) {
+        scope.hasChanged(scope, r);
+      });
+    })(this, r);
+    this.hasChanged(this, r);
+  },
+  hasChanged: function (scope, r) {
+    if (window.location.hash.length > 0) {
+      for (var i = 0, length = r.length; i < length; i++) {
+        var route = r[i];
+        if (route.isActiveRoute(window.location.hash.substr(1))) {
+          scope.goToRoute(route.htmlName);
         }
-    },
-    goToRoute: function (htmlName) {
-        (function(scope) {
-            var url = 'views/' + htmlName,
-                xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState === 4 && this.status === 200) {
-                    scope.rootElem.innerHTML = this.responseText;
-                }
-            };
-            xhttp.open('GET', url, true);
-            xhttp.send();
-        })(this);
+      }
+    } else {
+      for (var i = 0, length = r.length; i < length; i++) {
+        var route = r[i];
+        if (route.default) {
+          scope.goToRoute(route.htmlName);
+        }
+      }
     }
+  },
+  goToRoute: function (htmlName, callback) {
+    (function (scope) {
+      var url = 'views/' + htmlName,
+        xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+          scope.rootElem.innerHTML = this.responseText;
+        }
+      };
+      xhttp.open('GET', url, true);
+      xhttp.send();
+      xhttp.onload = function (e) {
+        if (xhttp.readyState === 4) {
+          if (xhttp.status === 200) {
+            if (typeof callback === 'function') {
+              callback();
+            }
+          }
+        }
+      };
+    })(this);
+  },
 };
 
 export default Router;
