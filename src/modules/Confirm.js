4369f5eefff1ddfe0ad02eda7a5ac67e05a5bad7
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-11-11 22:18:55 +0300

    #61 Confirm class

--- src/modules/Confirm.js
+++ src/modules/Confirm.js
@@ -5,8 +5,15 @@ class Confirm {
   constructor(client, state) {
     this.client = client;
     this.state = state;
+    this.confirmCodeInput = '';
+    this.invalid = false;
   }
   sendConfirmCode(confirmCode) {
+    if(confirmCode.length > 0) {
+      this.confirmCodeInput.className = `confirm__input confirm__input_active ${this.invalid && 'confirm__input_error'}`;
+    } else {
+      this.confirmCodeInput.className = `confirm__input ${this.invalid && 'confirm__input_error'}`;
+    }
     if (confirmCode.length === 5) {
       console.log('SENDD!!!', confirmCode);
       this.client.send({
@@ -15,15 +22,17 @@ class Confirm {
       }).then(result => {
         console.log('result', result);
       }).catch(error => {
-        console.error(error);
+        this.confirmCodeInput.className = 'confirm__input confirm__input_error';
+        this.invalid = true;
       });
     }
   }
   render() {
     const confirmTitle = document.getElementById('confirmPhone');
-    const confirmCodeInput = document.getElementById('confirmCode');
+    this.confirmCodeInput = document.getElementById('confirmInput');
+    const confirmCode = document.getElementById('confirmCode');
     confirmTitle.innerText = storage.get('phone');
-    confirmCodeInput.addEventListener('keyup', () => this.sendConfirmCode(confirmCodeInput.value));
+    this.confirmCodeInput.addEventListener('keyup', () => this.sendConfirmCode(confirmCode.value));
   }
 }
 export default Confirm;
