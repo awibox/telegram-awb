0c67012fa7d22e12c0bdac094929dbd17081dbba
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-11-12 20:18:06 +0300

    #66: Password class was created

---
+++ src/modules/Password.js
@@ -0,0 +1,55 @@
+import 'styles/password.scss';
+
+class Password {
+  constructor(client, state) {
+    this.client = client;
+    this.state = state;
+    this.showPassword = false;
+    this.invalid = false;
+  }
+  sendPasswordCode(passwordCode, passwordCodeInput) {
+    console.log('SENDD PASS!!!', passwordCode);
+    this.client.send({
+      '@type': 'checkAuthenticationPassword',
+      password: passwordCode,
+    }).then(result => {
+      console.log('result', result);
+    }).catch(error => {
+      passwordCodeInput.className = 'password__input password__input_error';
+      this.invalid = true;
+    });
+  }
+  setLabel(passwordCode, passwordCodeInput, passwordEye) {
+    if(passwordCode.length > 0) {
+      passwordCodeInput.className = `password__input password__input_active ${this.invalid && 'password__input_error'}`;
+      passwordEye.style.display = 'block';
+    } else {
+      passwordCodeInput.className = `password__input ${this.invalid && 'password__input_error'}`;
+      passwordEye.style.display = 'none';
+    }
+  }
+  showPasswordCode(passwordEye, passwordImg, passwordCode) {
+    console.log('showPasswordCode');
+    if (this.showPassword) {
+      passwordEye.className = 'password__input-eye';
+      passwordImg.className = 'password__img';
+      passwordCode.type = 'password';
+    } else {
+      passwordEye.className = 'password__input-eye password__input-eye_active';
+      passwordImg.className = 'password__img password__img_show';
+      passwordCode.type = 'text';
+    }
+    this.showPassword = !this.showPassword;
+  }
+  render() {
+    const passwordCodeInput = document.getElementById('passwordInput');
+    const passwordEye = document.getElementById('passwordEye');
+    const passwordImg = document.getElementById('passwordImg');
+    const passwordCode = document.getElementById('passwordCode');
+    const passwordButton = document.getElementById('passwordButton');
+    passwordCodeInput.addEventListener('keyup', () => this.setLabel(passwordCode.value, passwordCodeInput, passwordEye));
+    passwordButton.addEventListener('click', () => this.sendPasswordCode(passwordCode.value, passwordCodeInput));
+    passwordEye.addEventListener('click', () => this.showPasswordCode(passwordEye, passwordImg, passwordCode));
+  }
+}
+export default Password;
