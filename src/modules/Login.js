740b0bcab67f45051c15c71e8bc702ca67c67947
Andrei Arkhipov <andrei@awb.pw>
2019-11-12 23:10:39 +0300

    Design fixes, build script etc

--- src/modules/Login.js
+++ src/modules/Login.js
@@ -5,38 +5,48 @@ class Login {
   constructor(client, state) {
     this.client = client;
     this.state = state;
+    this.isReadyForSending = true;
+    this.invalid = false;
   }
-  sendPhoneNumber(phoneNumber) {
-    console.log('this.client', this.client, 'phoneNumber', phoneNumber);
-    this.client.send({
-      '@type': 'setAuthenticationPhoneNumber',
-      phone_number: phoneNumber,
-    }).then(result => {
-      console.log('______ setAuthenticationPhoneNumber _______', result);
-    }).catch(error => {
-      console.error(error);
-    });
+  sendPhoneNumber(phoneNumber, phoneNumberInput, phoneNumberSendButton) {
+    if(this.isReadyForSending) {
+      phoneNumberSendButton.innerText = 'Wait...';
+      this.isReadyForSending = false;
+      setTimeout(() => {
+        this.isReadyForSending = true;
+        phoneNumberSendButton.innerText = 'NEXT';
+      }, 3000);
+      this.client.send({
+        '@type': 'setAuthenticationPhoneNumber',
+        phone_number: phoneNumber,
+      }).then(result => {
+        console.log('______ setAuthenticationPhoneNumber _______', result);
+      }).catch(error => {
+        phoneNumberInput.className = 'login__input login__input_error';
+        this.invalid = true;
+      });
+    }
   }
   onChangePhone(phoneNumber, phoneNumberInput) {
     console.log('phoneNumber', phoneNumber);
     storage.set('phone', phoneNumber);
     if (phoneNumber.length > 0) {
-      phoneNumberInput.className = `login__input login__input_active`;
+      phoneNumberInput.className = `login__input login__input_active ${this.invalid ? 'login__input_error' : ''}`;
     } else {
-      phoneNumberInput.className = `login__input`;
+      phoneNumberInput.className = `login__input ${this.invalid ? 'login__input_error' : ''}`;
     }
   }
   render() {
-    const phoneNumberInput = document.getElementById('phoneInput');
     const phoneNumber = document.getElementById('phoneNumber');
+    const phoneNumberInput = document.getElementById('phoneInput');
+    const phoneNumberSendButton = document.getElementById('phoneNumberButton');
     if(storage.get('phone')) {
       phoneNumber.value = storage.get('phone');
+      phoneNumberInput.className = `login__input login__input_active`;
     }
-    console.log('this.phoneNumberInput', this.phoneNumberInput);
-    const phoneNumberSendButton = document.getElementById('phoneNumberButton');
-
-    phoneNumberSendButton.addEventListener('click', () => this.sendPhoneNumber(phoneNumber.value));
+    phoneNumberSendButton.addEventListener('click', () => this.sendPhoneNumber(phoneNumber.value, phoneNumberInput, phoneNumberSendButton));
     phoneNumber.addEventListener('keyup', () => this.onChangePhone(phoneNumber.value, phoneNumberInput));
+
   }
 }
 export default Login;
